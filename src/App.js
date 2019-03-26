e2f37d11ff50858c6c1ea4b01020932588ea440c
Jachaplin <jakealanchaplin@yahoo.com>
2019-03-26 03:44:40 -0700

    new file structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Navbar from './components/navbar/Navbar'
+import Footer from './components/footer/Footer'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello World</h1>
+      <div>
+        <Navbar />
+        <Footer />
       </div>
     );
   }
